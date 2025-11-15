e6033ff6b08d27b73da276f1ae2d021531677c5f
Ivancabral610 <ivancabral1@icloud.com>
2025-11-15 17:33:01 -0300

    Probando agregar titulos e imagenes

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import pelis from "../movies.json";
 
 function App() {
-  return <></>;
+  const pelisTransformadas = pelis.map((peli) => (
+    <div>
+      <h1>{peli.title}</h1>
+      <img src={peli.poster_path} alt={peli.title} width="200" />
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>hola</h1>
+      {pelisTransformadas}
+    </div>
+  );
 }
 
 export default App;
