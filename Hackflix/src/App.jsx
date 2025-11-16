e8410c0bd7bd84a75613900da1af560d07ecdb37
Ivancabral610 <ivancabral1@icloud.com>
2025-11-16 19:42:13 -0300

    enlace boostrap e hice cards con las imagenes en

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
-import pelis from "../movies.json";
+import pelis from "../data.json";
 
 function App() {
-  const pelisTransformadas = pelis.map((peli) => (
-    <div>
-      <h1>{peli.title}</h1>
-      <img src={peli.poster_path} alt={peli.title} width="200" />
-    </div>
-  ));
-
   return (
-    <div>
-      <h1>hola</h1>
-      {pelisTransformadas}
+    <div className="container mt-4">
+      <div className="row">
+        {pelis.map((peli) => (
+          <div className="col-3" key={peli.id}>
+            <div className="card h-100">
+              <img
+                src={peli.poster_path}
+                className="card-img-top"
+                alt={peli.title}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
