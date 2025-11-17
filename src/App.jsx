add37b4691164ee48037f70290025cf6ca157e2f
trsh <trshdesigns@outlook.com>
2025-11-17 00:52:36 -0300

    added boostrap and changed the layout of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
+
 import pelis from "./assets/json/movies.json";
 
-function App() {
-  const pelisTransformadas = pelis.map((peli) => (
-    <div>
-      <h1>{peli.title}</h1>
-      <img src={peli.poster_path} alt={peli.title} width="200" />
-    </div>
-  ));
-
+function MovieCard({ movie }) {
   return (
-    <div>
-      <h1>hola</h1>
-      {pelisTransformadas}
+    <div className="movie-card">
+      <h3>{movie.title}</h3>
+      <div className="">
+        <img
+          src={movie.poster_path}
+          alt={movie.title}
+          width="150"
+          className="movie-poster"
+        />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="movies-grid">
+      {pelis.map((movie, index) => (
+        <MovieCard key={movie.id || `${movie.title}-${index}`} movie={movie} />
+      ))}
     </div>
   );
 }
