7e9632fe7e7932ebd28c0031ca0308c067376398
trsh <trshdesigns@outlook.com>
2025-11-18 16:32:19 -0300

    created component for movies and added css rules for stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import "./App.css";
-
 import pelis from "./assets/json/movies.json";
-import { Rating } from "react-simple-star-rating";
-
-function MovieCard({ movie }) {
-  return (
-    <div className="movie-card">
-      <h3>{movie.title}</h3>
-      <div className="">
-        <img
-          src={movie.poster_path}
-          alt={movie.title}
-          width="150"
-          className="movie-poster"
-        />
-      </div>
-    </div>
-  );
-}
+import MovieCard from "./components/Moviecard";
 
 function App() {
   return (
     <div>
       <div className="rating-container">
-        <Rating></Rating>
       </div>
       <div className="movies-grid">
         {pelis.map((movie, index) => (
@@ -32,7 +14,6 @@ function App() {
             movie={movie}
           />
         ))}
-        <Rating></Rating>
       </div>
     </div>
   );
---
+++ src/components/Moviecard.jsx
@@ -0,0 +1,20 @@
+import { Rating } from "react-simple-star-rating";
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <h3>{movie.title}</h3>
+      <div className="movie-card-container">
+        <img
+          src={movie.poster_path}
+          alt={movie.title}
+          width="150"
+          className="movie-poster"
+        />
+        <Rating></Rating>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard
