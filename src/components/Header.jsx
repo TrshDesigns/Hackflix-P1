adec7f90797ca0f2d96dcc459f666458913ad7a0
trsh <trshdesigns@outlook.com>
2025-11-18 21:50:44 -0300

    Organizing navbar and header in order to execute figma design
    Co-authored-by: Ivancabral610 <Ivancabral610@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,19 @@
-// Import Bootstrap's CSS
-function Header(){
-    return(
-        <>
-        <header className="p-3 bg-dark text-white">
-    <div className="container">
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-        </a>
+import { Carousel } from "nuka-carousel";
+import Navbar from "./Navbar";
 
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">About</a></li>
-        </ul>
-
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-        </form>
-
-        <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2">Login</button>
-          <button type="button" className="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
+function Header() {
+  return (
+    <>
+    <div>
+      <Navbar></Navbar>
     </div>
-  </header>
-        </>
-    )
+      <Carousel autoplay showDots>
+        <div>Slide 1</div>
+        <div>Slide 2</div>
+        <div>Slide 3</div>
+      </Carousel>
+    </>
+  );
 }
 
 export default Header
