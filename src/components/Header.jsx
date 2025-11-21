1e00a3530c9e118627de2ddba302923df4a1d5d5
Ivancabral610 <ivancabral1@icloud.com>
2025-11-21 18:06:48 -0300

    push obligatorio
    Signed-off-by: Ivancabral610 <ivancabral1@icloud.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,19 @@
 import { Carousel } from "nuka-carousel";
 import Navbar from "./Navbar";
-import "./Header.css";
+
+//PENDIENTE INSERTAR IMAGENES PARA CAROUSEL
+//LEER DOCUMENTACION DE NUKA CAROUSEL
+
 function Header() {
   return (
     <>
       <div>
         <Navbar></Navbar>
       </div>
-      <Carousel
-        autoplay
-        showDots
-        wrapAround
-        pauseOnHover
-        autoplayInterval={3000}
-      >
-        <div className="slide">
-          <img src="src/media/header001.png" alt="Banner1" />
-        </div>
-        <div>Slide 2</div>
-        <div>Slide 3</div>
+      <Carousel autoplay showDots>
+        <div className="header-carousel-div"></div>
+        <div className="header-carousel-div"></div>
+        <div className="header-carousel-div"></div>
       </Carousel>
     </>
   );
