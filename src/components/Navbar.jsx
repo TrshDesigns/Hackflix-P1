98acf0a2a94fd3742d25c4993beb9411fdbdc5c3
Ivancabral610 <ivancabral1@icloud.com>
2025-11-21 22:26:35 -0300

    cambio nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,57 @@
-import "./nav.css"
+import "./nav.css";
 
 function Navbar() {
   return (
     <>
-        <div className="nav-container navbar-glass p-3 text-white">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <div className="nav-container navbar-glass p-3 text-white">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none navbar-logo"
+          >
+            <svg className="bi me-2" width="40" height="32" role="img"></svg>
+          </a>
 
-            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none navbar-logo">
-              <svg className="bi me-2" width="40" height="32" role="img"></svg>
-            </a>
+          <div className="text-star">
+            <button
+              type="button"
+              className="btn btn-outline-light btn-glass me-2"
+            >
+              LOGO
+            </button>
+          </div>
 
-            <div className="text-star">
-              <button type="button" className="btn btn-outline-light btn-glass me-2">LOGO</button>
-            </div>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="#" className="nav-link px-3 nav-item-glass">
+                Bienvenida
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-3 nav-item-glass">
+                Planes
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link px-3 nav-item-glass">
+                FAQs
+              </a>
+            </li>
+          </ul>
 
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" className="nav-link px-3 nav-item-glass">Bienvenida</a></li>
-              <li><a href="#" className="nav-link px-3 nav-item-glass">Planes</a></li>
-              <li><a href="#" className="nav-link px-3 nav-item-glass">FAQs</a></li>
-            </ul>
-
-            <div className="text-end">
-              <button type="button" className="btn btn-outline-light btn-glass me-2">Login</button>
-              <button type="button" className="btn btn-warning btn-glass">Sign-up</button>
-            </div>
+          <div className="text-end">
+            <button
+              type="button"
+              className="btn btn-outline-light btn-glass me-2"
+            >
+              Login
+            </button>
+            <button type="button" className="btn btn-warning btn-glass">
+              Sign-up
+            </button>
           </div>
         </div>
+      </div>
     </>
   );
 }
